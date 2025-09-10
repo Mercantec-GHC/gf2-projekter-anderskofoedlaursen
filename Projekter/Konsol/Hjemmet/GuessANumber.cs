ca24353a80d9e795dc9515a8a047378b351e811c
anderskofoedlaursen <anders.kofoed.laursen@gmail.com>
2025-09-10 14:52:40 +0200

    Update GuessANumber.cs
    første forsøg

--- Projekter/Konsol/Hjemmet/GuessANumber.cs
+++ Projekter/Konsol/Hjemmet/GuessANumber.cs
@@ -5,6 +5,9 @@ namespace Hjemmet
         public void Start()
         {
             Console.WriteLine("Gæt et tal er ikke implementeret endnu.");
+            Random ellasfingrer = new Random();
+            int ellasfingrer1 = ellasfingrer.Next(1, 10);
+            Console.WriteLine($"pssst, Ella er ikke god til det med fingrer: {ellasfingrer}");
             Console.ReadKey();
         }
     }
